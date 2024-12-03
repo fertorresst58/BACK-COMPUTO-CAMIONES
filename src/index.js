8a13a080a6d08222321701b76f70f1dd00a8bd77
fertorresst58 <fernan.trf.torres@gmail.com>
2024-12-03 13:08:49 -0600

    CONFIGURACION DE PUERTO

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/', routes)
 
-const PORT = process.env.PORT || 5010
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`ESCUCHANDO EN EL PUERTO: ${PORT}`)
 })
